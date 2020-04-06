34517f0728454a478478fd9fdc2a8bb9483eddfa
Константинов Максим <konstantinovmax75@gmail.com>
2020-04-06 20:20:44 +0700

    Add project files.

---
+++ OpenGlWpf1/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SharpGL;
+using SharpGL.SceneGraph;
+
+namespace OpenGlWpf1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenGLControl_OnOpenGLDraw(object sender, OpenGLEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OpenGlControl_OpenGlInitialized(object sender, OpenGLEventArgs args)
+        {
+            OpenGL gl = args.OpenGL;
+        }
+    }
+}
---
+++ OpenGlWpf1/Scene/Scene.cs
@@ -0,0 +1,11 @@
+﻿using SharpGL.Shaders;
+
+namespace OpenGlWpf1.Scene
+{
+    class Scene
+    {
+        private ShaderProgram _shaderPixel;
+
+        public void Initialize
+    }
+}
